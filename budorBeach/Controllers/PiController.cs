342db927f818c62038329febfa59a784db916c36
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-27 11:43:25 +0100

    Add TakePicture endpoint

---
+++ budorBeach/Controllers/PiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MMALSharp;
+using MMALSharp.Common;
+using MMALSharp.Handlers;
+
+namespace budorBeach.Controllers
+{
+    [Route("api/PiController")]
+    [ApiController]
+    public class PiController : ControllerBase
+    {
+        [HttpPost]
+        public async Task TakePicture()
+        {
+            var cam = MMALCamera.Instance;
+
+            using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))
+            {
+                await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+            }
+
+            cam.Cleanup();
+        }
+    }
+}
