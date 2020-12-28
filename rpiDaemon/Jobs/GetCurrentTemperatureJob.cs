b1ac58cec74392eb7eff0158a14d6dea9bedb88c
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-29 00:09:13 +0100

    Add Quartz scheduler, remove budorBeach project

---
+++ rpiDaemon/Jobs/GetCurrentTemperatureJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace rpiDaemon.Jobs
+{
+    public class GetCurrentTemperatureJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
