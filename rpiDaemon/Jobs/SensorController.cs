da93dfe865951b80806d557149356e7ccf03cb63
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-28 23:36:31 +0100

    Add rpiDaemon project

---
+++ rpiDaemon/Jobs/SensorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpiDaemon.Controllers
+{
+    public class SensorController
+    {
+
+        public Tuple<DateTime, double> GetCurrentTemperature()
+        {
+            return new Tuple<DateTime, double>(DateTime.UtcNow, default);
+        }
+
+
+    }
+}
