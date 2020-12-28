ba0dadf277a5b775443e890b7aea051460f8ffbd
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-29 00:09:13 +0100

    Add Quartz scheduler, remove budorBeach project

--- rpiDaemon/Jobs/TakePictureJob.cs
+++ rpiDaemon/Jobs/TakePictureJob.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using Quartz;
 
 namespace rpiDaemon.Jobs
 {
+    [UsedImplicitly]
     public class TakePictureJob : IJob
     {
         private readonly ILogger<TakePictureJob> _logger;
@@ -20,9 +22,17 @@ namespace rpiDaemon.Jobs
 
         public Task Execute(IJobExecutionContext context)
         {
-            Console.WriteLine($"Taking image at time {DateTime.UtcNow}");
             _logger.LogInformation($"Taking image at time {DateTime.UtcNow}");
             return Task.FromResult(true);
+
+            //var cam = MMALCamera.Instance;
+
+            //using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))
+            //{
+            //    await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+            //}
+
+            //cam.Cleanup();
         }
     }
 }
