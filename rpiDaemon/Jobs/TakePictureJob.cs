15c09d674b792b28ff86546b5469cdca6d98adb0
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-28 23:36:31 +0100

    Add rpiDaemon project

---
+++ rpiDaemon/Jobs/TakePictureJob.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace rpiDaemon.Jobs
+{
+    public class TakePictureJob : IJob
+    {
+        private readonly ILogger<TakePictureJob> _logger;
+
+        public TakePictureJob()
+        {
+        }
+
+        public TakePictureJob(ILogger<TakePictureJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Taking image at time {DateTime.UtcNow}");
+            _logger.LogInformation($"Taking image at time {DateTime.UtcNow}");
+            return Task.FromResult(true);
+        }
+    }
+}
