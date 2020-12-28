f57d035af800d347287bd9b638f10c6bbb4a0067
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-28 23:36:31 +0100

    Add rpiDaemon project

---
+++ rpiDaemon/Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+using rpiDaemon.Jobs;
+
+namespace rpiDaemon
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddQuartz(q =>
+            {
+                var jobKey = new JobKey(nameof(TakePictureJob), "secondJobs");
+                q.AddJob<TakePictureJob>(j => j.StoreDurably());
+                q.AddTrigger(t => t
+                    .WithIdentity("secondly trigger")
+                    .ForJob(jobKey)
+                    .StartNow()
+                    .WithSimpleSchedule(s => s.WithInterval(TimeSpan.FromSeconds(2)).RepeatForever()));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            });
+        }
+    }
+}
