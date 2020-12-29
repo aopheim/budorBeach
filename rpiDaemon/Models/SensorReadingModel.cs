c563e4e901caaaac0f244b6b1314fe4084ce0c28
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-29 19:00:36 +0100

    Add SQL Server db with sensor readings

---
+++ rpiDaemon/Models/SensorReadingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace rpiDaemon.Models
+{
+    public class SensorReadingModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime MeasuredAtUtc { get; set; }
+        public double TemperatureInDegreesC { get; set; }
+    }
+}
