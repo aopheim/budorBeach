792947067ee5edc16a71a8bb5c3dedfb833ca3da
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-30 23:27:30 +0100

    Add pressure readings with serial communication

---
+++ rpiDaemon/Migrations/20201230221643_AddPressure.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rpiDaemon.Migrations
+{
+    public partial class AddPressure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "PressureInKPa",
+                table: "SensorReadings",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PressureInKPa",
+                table: "SensorReadings");
+        }
+    }
+}
