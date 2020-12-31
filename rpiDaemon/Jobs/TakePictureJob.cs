c95b75f73207bb073509d3327e759bb400df97ba
Adrian Opheim <adrian.opheim@fiizk.com>
2020-12-31 17:32:47 +0100

    Upload images to Azure Blob Storage

--- rpiDaemon/Jobs/TakePictureJob.cs
+++ rpiDaemon/Jobs/TakePictureJob.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MMALSharp;
 using MMALSharp.Common;
@@ -12,27 +15,33 @@ namespace rpiDaemon.Jobs
     [UsedImplicitly]
     public class TakePictureJob : IJob
     {
+        private const string BlobContainerName = "images";
+        private readonly IConfiguration _config;
+        private readonly BlobContainerClient _containerClient;
         private readonly ILogger<TakePictureJob> _logger;
 
         public TakePictureJob()
         {
         }
 
-        public TakePictureJob(ILogger<TakePictureJob> logger)
+        public TakePictureJob(ILogger<TakePictureJob> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+            _containerClient =
+                new BlobContainerClient(_config.GetConnectionString("AzureStorageConnectionString"), BlobContainerName);
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
+            var now = DateTime.UtcNow;
+            var folderName = $"{now.Year}-{now.Month}-{now.Day}";
+            var fileName = $"{now.Hour}-{now.Minute}-{now.Second}";
+            var fullPath = $"/home/pi/images/{folderName}/{fileName}.jpg";
             try
             {
-                var now = DateTime.UtcNow;
-                var folderName = $"{now.Year}-{now.Month}-{now.Day}";
-
                 var cam = MMALCamera.Instance;
-
-                using (var imgCaptureHandler = new ImageStreamCaptureHandler($"/home/pi/images/{folderName}", "jpg"))
+                using (var imgCaptureHandler = new ImageStreamCaptureHandler(fullPath))
                 {
                     await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
                 }
@@ -45,6 +54,11 @@ namespace rpiDaemon.Jobs
                 else
                     throw;
             }
+
+            var blobClient = _containerClient.GetBlobClient($"/{folderName}/{fileName}.jpg");
+            await using var uploadFileStream = File.OpenRead(fullPath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
         }
     }
 }
