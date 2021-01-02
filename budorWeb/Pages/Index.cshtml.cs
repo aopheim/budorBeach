bcdf63ada37770ce9a1964a8292840fb1127a9ce
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-02 19:49:33 +0100

    Add budorWeb project

---
+++ budorWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace budorWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private string _newestImageUrl;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
