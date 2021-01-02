41e7a9cf2a52b9c189d92a1cfc382d9b4ae0b2b1
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-02 19:19:39 +0100

    Add DateTimeParsing and DeleteLocalImagesJob. Add Test project

---
+++ budorBeach/Jobs/DeleteLocalImagesJob.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Quartz;
+using rpiDaemon.DateTimeHelpers;
+
+namespace rpiDaemon.Jobs
+{
+    public class DeleteLocalImagesJob : IJob
+    {
+        private const int NumberOfDaysCutOff = 30;
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            var cutOffDate = DateTime.UtcNow.AddDays(-NumberOfDaysCutOff);
+
+            var folderNames = Directory.GetDirectories("/home/pi/images/");
+
+            foreach (var fullPath in folderNames)
+            {
+                var folderName = Path.GetDirectoryName(fullPath);
+                var folderDate = DateTimeParser.GetDateTimeFromFolderName(folderName);
+
+                if (folderDate > cutOffDate)
+                    continue;
+
+                var dir = new DirectoryInfo(fullPath);
+                dir.Delete();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
