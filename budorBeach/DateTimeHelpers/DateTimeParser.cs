6f512f485513d451b09982592ce97b763d61eda5
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-02 19:19:39 +0100

    Add DateTimeParsing and DeleteLocalImagesJob. Add Test project

---
+++ budorBeach/DateTimeHelpers/DateTimeParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace rpiDaemon.DateTimeHelpers
+{
+    public static class DateTimeParser
+    {
+        public static string GetFolderName(DateTime date)
+        {
+            return $"{date.Year}-{date.Month}-{date.Day}";
+        }
+
+        public static string GetFileName(DateTime date)
+        {
+            return $"{date.Hour}-{date.Minute}-{date.Second}";
+        }
+
+        public static DateTime GetDateTimeFromFolderName(string folderName)
+        {
+            var split = folderName.Split('-');
+            return split.Length != 3
+                ? default
+                : new DateTime(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2]), 00, 00, 00);
+        }
+    }
+}
