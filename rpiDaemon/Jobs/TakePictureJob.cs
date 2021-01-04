a96babcc85fb61519f3d9c8603ed790cca39c285
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-04 21:58:31 +0100

    ISO 800

--- rpiDaemon/Jobs/TakePictureJob.cs
+++ rpiDaemon/Jobs/TakePictureJob.cs
@@ -46,7 +46,7 @@ namespace rpiDaemon.Jobs
                 using (var imgCaptureHandler = new ImageStreamCaptureHandler(fullPath))
                 {
                     MMALCameraConfig.StillResolution = Resolution.As1MPixel;
-                    MMALCameraConfig.ISO = 2000;
+                    MMALCameraConfig.ISO = 800;
                     MMALCameraConfig.Debug = true;
                     await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
                 }
