38bb271fe5ba0e7714a76792ec6ca447f608f10d
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-05 19:42:33 +0100

    Add CameraTest project

---
+++ CameraTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using MMALSharp;
+using MMALSharp.Common;
+using MMALSharp.Handlers;
+using rpiDaemon.DateTimeHelpers;
+
+namespace CameraTest
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var now = DateTime.UtcNow;
+            var folderName = DateTimeParser.GetFolderName(now);
+            var fileName = DateTimeParser.GetFileName(now);
+            var fullPath = $"/home/pi/images/{folderName}/{fileName}.jpg";
+
+            for (var i = 0; i < 10; i++)
+            {
+                var camera = MMALCamera.Instance;
+                MMALCameraConfig.Debug = true;
+
+                using var imgCaptureHandler = new ImageStreamCaptureHandler(fullPath);
+                await camera.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+            }
+        }
+    }
+}
