d7b031a35b7d7c135fdeb8c37ced7e09b8ead10c
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-27 10:50:11 +0100

    Push Bme280 sensor readings to db. Add migrations

---
+++ rpiDaemon/Migrations/20210127094403_UpdateSensorReadingModelWithBme280Support.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rpiDaemon.Migrations
+{
+    public partial class UpdateSensorReadingModelWithBme280Support : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PressureInKPa",
+                table: "SensorReadings",
+                newName: "RelativeHumidityInPercent");
+
+            migrationBuilder.AddColumn<double>(
+                name: "AlititudeInMeters",
+                table: "SensorReadings",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PressureInhPa",
+                table: "SensorReadings",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlititudeInMeters",
+                table: "SensorReadings");
+
+            migrationBuilder.DropColumn(
+                name: "PressureInhPa",
+                table: "SensorReadings");
+
+            migrationBuilder.RenameColumn(
+                name: "RelativeHumidityInPercent",
+                table: "SensorReadings",
+                newName: "PressureInKPa");
+        }
+    }
+}
