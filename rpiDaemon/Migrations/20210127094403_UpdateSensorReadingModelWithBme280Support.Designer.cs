2996848423e7a2a750ef92406a6688086c4cb728
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-27 10:50:11 +0100

    Push Bme280 sensor readings to db. Add migrations

--- rpiDaemon/Migrations/20210127094403_UpdateSensorReadingModelWithBme280Support.Designer.cs
+++ rpiDaemon/Migrations/20210127094403_UpdateSensorReadingModelWithBme280Support.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using rpiDaemon;
 
 namespace rpiDaemon.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class SensorContextModelSnapshot : ModelSnapshot
+    [Migration("20210127094403_UpdateSensorReadingModelWithBme280Support")]
+    partial class UpdateSensorReadingModelWithBme280Support
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -26,7 +28,7 @@ namespace rpiDaemon.Migrations
                         .HasColumnType("int")
                         .UseIdentityColumn();
 
-                    b.Property<double>("AltitudeInMeters")
+                    b.Property<double>("AlititudeInMeters")
                         .HasColumnType("float");
 
                     b.Property<DateTime>("MeasuredAtUtc")
---
+++ rpiDaemon/Migrations/20210127094815_RenameAltitude.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rpiDaemon.Migrations
+{
+    public partial class RenameAltitude : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AlititudeInMeters",
+                table: "SensorReadings",
+                newName: "AltitudeInMeters");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AltitudeInMeters",
+                table: "SensorReadings",
+                newName: "AlititudeInMeters");
+        }
+    }
+}
