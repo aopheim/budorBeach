84f6c978ccc62470a2fc147a6e3c78c4f4ad167e
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-28 12:02:47 +0100

    Remove debug logging. Image every 5 min, sensor reading every 20 second.

--- rpiDaemon/Jobs/GetBme280SensorReadingsJob.cs
+++ rpiDaemon/Jobs/GetBme280SensorReadingsJob.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Device.I2c;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Iot.Device.Bmxx80;
 using Iot.Device.Bmxx80.PowerMode;
 using JetBrains.Annotations;
-using Microsoft.Extensions.Logging;
 using Quartz;
 using rpiDaemon.Models;
 
@@ -17,20 +15,15 @@ namespace rpiDaemon.Jobs
     {
         private readonly ApplicationDbContext _context;
 
-        public GetBme280SensorReadingsJob(ApplicationDbContext context, ILogger<GetBme280SensorReadingsJob> logger)
+        public GetBme280SensorReadingsJob(ApplicationDbContext context)
         {
             _context = context;
-            _logger = logger;
         }
-
-        private ILogger<GetBme280SensorReadingsJob> _logger { get; }
 
         public async Task Execute(IJobExecutionContext jobExecutionContext)
         {
-            _logger.LogDebug("Starting execute sensor readings");
             var sensorReadingModel = GetCurrentSensorReadings();
 
-            _logger.LogInformation($"{JsonSerializer.Serialize(sensorReadingModel)}");
             _context.SensorReadings.Add(sensorReadingModel);
             await _context.SaveChangesAsync(jobExecutionContext.CancellationToken);
         }
@@ -38,12 +31,9 @@ namespace rpiDaemon.Jobs
         private SensorReadingModel GetCurrentSensorReadings()
         {
             var i2CSettings = new I2cConnectionSettings(1, Bmx280Base.DefaultI2cAddress);
-            _logger.LogDebug("i2c settings set");
             using var i2CDevice = I2cDevice.Create(i2CSettings);
-            _logger.LogDebug("i2cDevice created");
             using var bme280 = new Bme280(i2CDevice);
 
-            _logger.LogDebug("bme280 instantiated");
             var measurementTime = bme280.GetMeasurementDuration();
             bme280.SetPowerMode(Bmx280PowerMode.Forced);
             Thread.Sleep(measurementTime);
@@ -53,7 +43,6 @@ namespace rpiDaemon.Jobs
             bme280.TryReadHumidity(out var humValue);
             bme280.TryReadAltitude(out var altValue);
 
-            _logger.LogDebug($"Values read: {altValue}");
             return new SensorReadingModel
             {
                 MeasuredAtUtc = DateTime.UtcNow,
