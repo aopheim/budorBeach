085d80235c6784748e06e325378c5ccb436b714c
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-28 11:42:39 +0100

    Add logging to Bme280 job

--- rpiDaemon/Jobs/GetBme280SensorReadingsJob.cs
+++ rpiDaemon/Jobs/GetBme280SensorReadingsJob.cs
@@ -27,6 +27,7 @@ namespace rpiDaemon.Jobs
 
         public async Task Execute(IJobExecutionContext jobExecutionContext)
         {
+            _logger.LogDebug("Starting execute sensor readings");
             var sensorReadingModel = GetCurrentSensorReadings();
 
             _logger.LogInformation($"{JsonSerializer.Serialize(sensorReadingModel)}");
@@ -37,9 +38,12 @@ namespace rpiDaemon.Jobs
         private SensorReadingModel GetCurrentSensorReadings()
         {
             var i2CSettings = new I2cConnectionSettings(1, Bmx280Base.DefaultI2cAddress);
+            _logger.LogDebug("i2c settings set");
             using var i2CDevice = I2cDevice.Create(i2CSettings);
+            _logger.LogDebug("i2cDevice created");
             using var bme280 = new Bme280(i2CDevice);
 
+            _logger.LogDebug("bme280 instantiated");
             var measurementTime = bme280.GetMeasurementDuration();
             bme280.SetPowerMode(Bmx280PowerMode.Forced);
             Thread.Sleep(measurementTime);
@@ -49,6 +53,7 @@ namespace rpiDaemon.Jobs
             bme280.TryReadHumidity(out var humValue);
             bme280.TryReadAltitude(out var altValue);
 
+            _logger.LogDebug($"Values read: {altValue}");
             return new SensorReadingModel
             {
                 MeasuredAtUtc = DateTime.UtcNow,
