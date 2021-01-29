7b806a2091baaac459dbc3062e4d4d745701c348
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-29 14:24:37 +0100

    Not using localhost

--- rpiDaemon/Jobs/GetBme280SensorReadingsJob.cs
+++ rpiDaemon/Jobs/GetBme280SensorReadingsJob.cs
@@ -26,7 +26,7 @@ namespace rpiDaemon.Jobs
             _context = context;
             _logger = logger;
             _connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:3000/budorhub")
+                .WithUrl("http://127.0.0.1:3000/budorhub")
                 .WithAutomaticReconnect()
                 .Build();
 
