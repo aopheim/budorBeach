afbfd1c7e7cd88a282041e8c8c88407c0fa0c09c
Adrian Opheim <adrian.opheim@fiizk.com>
2021-01-29 11:46:43 +0100

    Add first client and Hub methods

--- budorWeb/Hubs/BudorHub.cs
+++ budorWeb/Hubs/BudorHub.cs
@@ -1,18 +1,23 @@
 ﻿using System.Threading.Tasks;
+using budorWeb.Interfaces;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.SignalR;
 
 namespace budorWeb.Hubs
 {
-    public class BudorHub : Hub
+    [UsedImplicitly]
+    public class BudorHub : Hub<IBudorWebClient>
     {
         public async Task SendMessageToAllClients(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message);
+            await Clients.All.ConsoleLogMessage(message);
         }
 
-        //public async Task GetUtcNow()
-        //{
-        //    return await Task.FromResult(DateTime.UtcNow);
-        //}
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.ConsoleLogMessage("SignalR is connected!");
+
+            await base.OnConnectedAsync();
+        }
     }
 }
