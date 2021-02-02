187e42a75f7da140607c1de4b165a556d6b605b2
Adrian Opheim <adrian.opheim@fiizk.com>
2021-02-02 15:49:32 +0100

    Add .NET SignalR Client to BudorWeb

--- budorWeb/Interfaces/IBudorWebClient.cs
+++ budorWeb/Interfaces/IBudorWebClient.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
+using rpiDaemon.Models;
 
 namespace budorWeb.Interfaces
 {
     public interface IBudorWebClient
     {
         Task ConsoleLogMessage(string message);
+        Task ReceiveCurrentSensorReading(SensorReadingModel model);
     }
 }
