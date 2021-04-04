348861d86fbc4199286c94a4dce6a50db6ee067e
Adrian Opheim <adrian.opheim@fiizk.com>
2021-04-04 18:53:08 +0200

    Sensor reading every 10 seconds

--- rpiDaemon/Startup.cs
+++ rpiDaemon/Startup.cs
@@ -41,7 +41,7 @@ namespace rpiDaemon
                 q.AddTrigger(t => t.WithIdentity("bme280Trigger")
                     .ForJob(bme280JobKey)
                     .StartAt(DateTime.UtcNow.AddSeconds(10))
-                    .WithSimpleSchedule(s => s.WithInterval(TimeSpan.FromSeconds(5)).RepeatForever()));
+                    .WithSimpleSchedule(s => s.WithInterval(TimeSpan.FromSeconds(10)).RepeatForever()));
 
                 q.UseMicrosoftDependencyInjectionScopedJobFactory();
             });
