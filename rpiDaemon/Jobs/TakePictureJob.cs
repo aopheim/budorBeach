d07089202d3599c5a7044f6bf6acd71a7842fc0a
Adrian Opheim <adrian.opheim@fiizk.com>
2021-04-09 13:07:17 +0200

    Decrease image interval

--- rpiDaemon/Jobs/TakePictureJob.cs
+++ rpiDaemon/Jobs/TakePictureJob.cs
@@ -48,7 +48,7 @@ namespace rpiDaemon.Jobs
             var sunrise = solarTimes.Sunrise;
             var sunset = solarTimes.Sunset;
 
-            if (now > sunrise.AddHours(-1) && now < sunset.AddHours(1))
+            if (now > sunrise.AddMinutes(-15) && now < sunset.AddMinutes(30))
                 await TakePictureJobHelper.TakeImageAndUploadAsync(_environment, _camera, _logger, _containerClient,
                     new PiCameraSettings());
         }
