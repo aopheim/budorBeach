3ac86b214a85d7eee4daa3d2f61ea38fe3695ede
Adrian Opheim <adrian.opheim@fiizk.com>
2021-05-30 18:44:04 +0200

    Add simple Images page for displaying all images for today

---
+++ budorWeb/Helpers/AzureStorageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace budorWeb.Helpers
+{
+    public static class AzureStorageHelper
+    {
+        public static BlobContainerClient GetBlobContainerClient(IConfiguration config, string blobContainerName)
+        {
+            var blobServiceClient = new BlobServiceClient(config.GetConnectionString("AzureStorageConnectionString"));
+            return blobServiceClient.GetBlobContainerClient(blobContainerName);
+        }
+
+        public static string GetUrlForBlob(this BlobItem blob, BlobContainerClient containerClient)
+        {
+            return new Uri(containerClient.Uri, $"{containerClient.Name}/{blob.Name}")
+                .ToString();
+        }
+    }
+}
