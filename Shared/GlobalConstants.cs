b3536cd880b197088a8a007c95fe6766cfd94e7c
Adrian Opheim <adrian.opheim@fiizk.com>
2021-05-31 18:08:47 +0200

    Add compression of uploaded images

---
+++ Shared/GlobalConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared
+{
+    public static class GlobalConstants
+    {
+        public const string ImagesContainerName = "images";
+        public const string ThumbnailImagesContainerName = "images-thumbnails";
+    }
+}
