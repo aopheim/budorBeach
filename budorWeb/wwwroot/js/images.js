838ea9e9981d75bdafccc02761dfa1d9d894f5dc
Adrian Opheim <adrian.opheim@fiizk.com>
2021-05-31 09:00:41 +0200

    Revert "Revert "Add simple Images page for displaying all images for today""
    This reverts commit 0624b5dbb5f1e30a468a487bb6981ea3db399522.

---
+++ budorWeb/wwwroot/js/images.js
@@ -0,0 +1,17 @@
+﻿function getImagesForDay(dateAsString) {
+    return fetch('./Index?handler=ImagesForDay&selectedDateAsString=' + dateAsString,
+        {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        });
+}
+
+$("#dateSelector").change(function () {
+    var dropDown = document.getElementById("dateSelector");
+    var dateAsString = dropDown.options[dropDown.selectedIndex].value;
+    getImagesForDay(dateAsString);
+});
+
+getChartData(12);
