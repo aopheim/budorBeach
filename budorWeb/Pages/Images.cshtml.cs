9598f47108521512afbd518d49445dd9e9e13a7b
Adrian Opheim <adrian.opheim@fiizk.com>
2021-05-31 18:08:47 +0200

    Add compression of uploaded images

--- budorWeb/Pages/Images.cshtml.cs
+++ budorWeb/Pages/Images.cshtml.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
-using budorWeb.Helpers;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using rpiDaemon.DateTimeHelpers;
+using Shared;
+using Shared.Azure;
 
 namespace budorWeb.Pages
 {
     public class ImagesModel : PageModel
     {
-        private const string ImagesBlobContainerName = "images";
         private readonly IConfiguration _config;
 
         public ImagesModel(IConfiguration config)
@@ -28,7 +28,8 @@ namespace budorWeb.Pages
             var today = DateTime.UtcNow;
             var folderName = DateTimeParser.GetFolderName(today);
 
-            var containerClient = AzureStorageHelper.GetBlobContainerClient(_config, ImagesBlobContainerName);
+            var containerClient =
+                AzureStorageHelper.GetBlobContainerClient(_config, GlobalConstants.ImagesContainerName);
             ImageBlobsForDay = containerClient.GetBlobs().Where(blob => blob.Name.Contains(folderName))
                 .OrderBy(blob => DateTimeParser.GetDateTimeFromFolderAndFileName(blob.Name)).ToList();
             ContainerClient = containerClient;
