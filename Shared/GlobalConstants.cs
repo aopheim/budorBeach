ac434d1e5ce4abe747c7ed9650804fc0eb0543d4
Adrian Opheim <adrian.opheim@fiizk.com>
2021-06-14 14:36:50 +0200

    Don't use budorbeach.no yet...

--- Shared/GlobalConstants.cs
+++ Shared/GlobalConstants.cs
@@ -5,7 +5,7 @@
         public const string ImagesContainerName = "images";
         public const string ThumbnailImagesContainerName = "images-thumbnails";
         public const string DevelopmentUrl = "http://localhost:3000";
-        public const string ProductionUrl = "https://budorbeach.no";
+        public const string ProductionUrl = "https://budorbeach.azurewebsites.net";
         public const string HubEndpoint = "/budorhub";
         public const string DevelopmentHubUrl = DevelopmentUrl + HubEndpoint;
         public const string ProductionHubUrl = ProductionUrl + HubEndpoint;
