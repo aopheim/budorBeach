bf1ca79c4a410e848297699150d471707ee4a8f2
Adrian Opheim <adrian.opheim@fiizk.com>
2021-06-18 13:57:11 +0200

    Add new feed items

--- budorWeb/Services/NewsFeedService.cs
+++ budorWeb/Services/NewsFeedService.cs
@@ -27,10 +27,36 @@ namespace budorWeb.Services
                     Id = 2,
                     Date = new DateTime(2021, 05, 1),
                     Header = "Fortsatt ingen besøk",
-                    Body = "Etter en måned er det dessverre fortsatt ingen besøkende i fuglekassa.",
+                    Body = "Etter en måned er det dessverre fortsatt ingen besøkende i fuglekassa. ",
                     ImageUrls = new List<string>
                     {
                         "https://budorbeach.blob.core.windows.net/images/2021-5-1/9-39-33.jpg"
+                    }
+                },
+                new NewsFeedEntry
+                {
+                    Id = 3,
+                    Date = new DateTime(2021, 05, 30),
+                    Header = "Første fugl har kommet!",
+                    Body =
+                        "Rett før kl 14 fikk fuglekassa sitt første besøk! Det ser ut som det var en vellykket visning - etter bare noen timer har det allerede kommet mye inventar på plass.",
+                    ImageUrls = new List<string>
+                    {
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2021-5-30/11-51-48.jpg",
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2021-5-31/5-52-4.jpg"
+                    },
+                },
+                new NewsFeedEntry
+                {
+                    Id = 4,
+                    Date = new DateTime(2021, 06, 01),
+                    Header = "Første bilde av innflytterne",
+                    Body =
+                        "Nå flyttes det inn for harde livet! Det ser ut som det er to forskjellige fugler som flytter inn i samme fuglekasse! Én grå og en mindre, svart.",
+                    ImageUrls = new List<string>
+                    {
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2021-6-1/4-45-5.jpg",
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2021-6-1/7-50-5.jpg"
                     }
                 }
             };
