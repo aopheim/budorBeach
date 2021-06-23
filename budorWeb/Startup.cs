b351cde3cdd1fc7ecc4480ff34a3c6f21f9514d6
Adrian Opheim <adrian.opheim@fiizk.com>
2021-06-23 23:31:01 +0200

    Revert "Comment out https redirection + Hsts"
    This reverts commit 6903db41cc2b8c37247f0e3ff253ccab8409d951.

--- budorWeb/Startup.cs
+++ budorWeb/Startup.cs
@@ -54,13 +54,17 @@ namespace budorWeb
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }
             else
+            {
                 app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //app.UseHsts();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
