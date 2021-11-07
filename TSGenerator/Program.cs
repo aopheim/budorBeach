018d6e2409a9c3c09edebee258380f31c41ca2d2
aopheim <37472517+aopheim@users.noreply.github.com>
2021-11-07 17:47:22 +0100

    Added TypeScript generation at build of budorWeb.Api

---
+++ TSGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NJsonSchema.CodeGeneration.TypeScript;
+using NSwag;
+using NSwag.CodeGeneration.TypeScript;
+
+namespace TSGenerator
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            Console.WriteLine("Generating TypeScript client and DTOs...");
+
+            const string swaggerFileName = "swagger.json";
+            var workingDir = Environment.CurrentDirectory;
+            var projectDirectory = Directory.GetParent(workingDir)?.FullName ?? "";
+            
+            var swaggerJsonPath = $"{projectDirectory}\\budorWeb.Api\\{swaggerFileName}";
+            var openApiDocument = await OpenApiDocument.FromFileAsync(swaggerJsonPath);
+            var settings = new TypeScriptClientGeneratorSettings
+            {
+                TypeScriptGeneratorSettings =
+                {
+                    TypeStyle = TypeScriptTypeStyle.Class
+                }
+            };
+
+            var generator = new TypeScriptClientGenerator(openApiDocument, settings);
+            var typeScriptServicesPath = $"{projectDirectory}\\budorWeb.Api\\ClientApp\\src\\services.ts";
+            var code = generator.GenerateFile();
+            await File.WriteAllTextAsync(typeScriptServicesPath, code);
+
+            Console.WriteLine("Generated TypeScript client at budorWeb.Api/ClientApp/src/services.ts");
+        }
+    }
+}
--- budorWeb.Api/Startup.cs
+++ budorWeb.Api/Startup.cs
@@ -38,6 +38,7 @@ namespace budorWeb.Api
             });
 
             services.AddSwaggerDocument();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
