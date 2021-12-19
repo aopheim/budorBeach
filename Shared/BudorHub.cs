54926431ea589e0875be693e6700a87b803607b6
Adrian Opheim <adrian.opheim@fiizk.com>
2021-12-19 18:25:50 +0100

    Use DI to get SignalRService

--- Shared/BudorHub.cs
+++ Shared/BudorHub.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using Shared.Interfaces;
 using Shared.Models;
 
@@ -9,24 +12,29 @@ namespace Shared
     [UsedImplicitly]
     public class BudorHub : Hub<IBudorHubClient>
     {
+        private readonly ILogger<BudorHub> _logger;
+
+        public BudorHub(ILogger<BudorHub> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task SendMessageToAllClients(string message)
         {
-            await Clients.All.ConsoleLogMessage(message);
+            var cTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await Clients.All.ConsoleLogMessage(message, cTokenSource.Token);
         }
 
         public async Task SendSensorReadingModelToWebClient(SensorReadingModel model)
         {
-            await Clients.All.ReceiveCurrentSensorReading(model);
+            var cTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await Clients.All.SendSensorReading(model, cTokenSource.Token);
         }
 
         public async Task TakeImage(PiCameraSettings.PiCameraSettings settings)
         {
-            await Clients.All.TakeImage(settings);
-        }
-
-        public override async Task OnConnectedAsync()
-        {
-            await base.OnConnectedAsync();
+            var cTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await Clients.All.TakeImage(settings, cTokenSource.Token);
         }
     }
 }
---
+++ Shared/SignalR/ISignalRService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Shared.Interfaces;
+
+namespace Shared.SignalR
+{
+    public interface ISignalRService : IBudorHubClient
+    {
+        public Task StartWithRetryAsync(CancellationToken cancellationToken);
+        public Task StopAsync(CancellationToken cancellationToken);
+        public void RegisterClientMethod<T>(string methodName, Action<T> action);
+    }
+}
