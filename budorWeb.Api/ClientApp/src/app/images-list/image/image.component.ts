0a1f8daa34afab4d54c5a6bb924708078ea302e8
Adrian Opheim <adrian.opheim@fiizk.com>
2022-01-18 09:15:21 +0100

    Fix Bootstrap set up for images-list

--- budorWeb.Api/ClientApp/src/app/images-list/image/image.component.ts
+++ budorWeb.Api/ClientApp/src/app/images-list/image/image.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input } from '@angular/core';
-import { BirdImage, IBirdImage } from '../images-list.component';
+import { Component, Input } from "@angular/core";
+import { BirdImage, IBirdImage } from "../images-list.component";
 
 @Component({
-  selector: 'bird-image',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  selector: "bird-image",
+  templateUrl: "./image.component.html",
+  styleUrls: ["./image.component.scss"],
 })
-
-export class ImageComponent  {
-  @Input() birdImage?: BirdImage
-
-
+export class ImageComponent {
+  @Input() birdImage?: BirdImage;
 }
--- budorWeb.Api/ClientApp/src/app/images-list/images-list.component.ts
+++ budorWeb.Api/ClientApp/src/app/images-list/images-list.component.ts
@@ -1,29 +1,50 @@
-import { Component, ModuleWithComponentFactories, OnInit } from '@angular/core';
+import { Component, ModuleWithComponentFactories, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-images-list',
-  templateUrl: './images-list.component.html',
-  styleUrls: ['./images-list.component.scss']
+  selector: "app-images-list",
+  templateUrl: "./images-list.component.html",
+  styleUrls: ["./images-list.component.scss"],
 })
-export class ImagesListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ImagesListComponent {
+  constructor() {
+    this.birdImages = [
+      new BirdImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+        "06.11.2021, 08:30:43"
+      ),
+      new BirdImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+        "06.11.2021, 08:30:43"
+      ),
+      new BirdImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+        "06.11.2021, 08:30:43"
+      ),
+      new BirdImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+        "06.11.2021, 08:30:43"
+      ),
+      new BirdImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+        "06.11.2021, 08:30:43"
+      ),
+      new BirdImage(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+        "06.11.2021, 08:30:43"
+      ),
+    ];
   }
 
-  birdImage = new BirdImage("https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg",
-   "06.11.2021, 08:30:43")
+  ngOnInit(): void {}
 
+  public birdImages: IBirdImage[];
 }
 
 export class BirdImage implements IBirdImage {
-  constructor(public url: string,  public date: string){
-  }
-
+  constructor(public url: string, public date: string) {}
 }
 
 export interface IBirdImage {
-  url: string,
-  date: string
+  url: string;
+  date: string;
 }
