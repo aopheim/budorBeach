3662810070fc7fe49f14df6e1bd58bd85d7721f1
aopheim <37472517+aopheim@users.noreply.github.com>
2022-05-03 23:03:52 +0200

    Add initial TakeVideoJob, and add BirdNetServer as a background service

--- Services/Interfaces/IBirdNetServer.cs
+++ Services/Interfaces/IBirdNetServer.cs
@@ -1,12 +1,12 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 
 namespace Services.Interfaces
 {
-    public interface IBirdNetServer
+    public interface IBirdNetServer : IHostedService
     {
-        Task<bool> StartBirdNetServer();
         Task<string> PostAsync(FileStream audioFileAsStream, CancellationToken cancellationToken);
     }
 }
--- Shared/GlobalConstants.cs
+++ Shared/GlobalConstants.cs
@@ -4,21 +4,27 @@
     {
         public const string ImagesContainerName = "images";
         public const string ThumbnailImagesContainerName = "images-thumbnails";
+
         public const string DevelopmentUrl = "http://localhost:3000";
         public const string ProductionUrl = "https://budorbeach.no";
         public const string HubEndpoint = "/budorhub";
         public const string DevelopmentHubUrl = DevelopmentUrl + HubEndpoint;
         public const string ProductionHubUrl = ProductionUrl + HubEndpoint;
+        public const string BirdNetServerUrl = "http://localhost:8080/analyze";
+
         public const string SecondJobs = "secondJobs";
         public const string DailyJobs = "dailyJobs";
+
         public const string PictureTrigger = "pictureTrigger";
         public const string ProximityTrigger = "proximityTrigger";
         public const string Bme280Trigger = "bme280Trigger";
         public const string AudioRecordingTrigger = "audioRecordingTrigger";
         public const string AudioAnalyzerTrigger = "audioAnalyzerTrigger";
-        public const string BirdNetServerUrl = "http://localhost:8080/analyze";
+        public const string VideoRecordingTrigger = "videoRecordingTrigger";
+
         public const double BudorLatitude = 60.974951;
         public const double BudorLongitude = 11.285140;
+
         public const string AudioRecordingsFolderWindows = @"C:\Users\AdrianOpheim\Documents\budorBeach\AudioService\";
         public const string AudioRecordingsFolderLinux = @"home\pi\audioRecordings\";
     }
