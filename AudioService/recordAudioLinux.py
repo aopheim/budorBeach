f35c15678a764e8dd5bd814bcbdf6a89665f0cec
aopheim <37472517+aopheim@users.noreply.github.com>
2022-05-04 23:22:21 +0200

    Record 10 seconds

--- AudioService/recordAudioLinux.py
+++ AudioService/recordAudioLinux.py
@@ -24,7 +24,7 @@ while True:
     chans = 1 # 1 channel
     samp_rate = 44100 # 44.1kHz sampling rate
     chunk = 4096 # 2^12 samples for buffer
-    record_secs = 5 # seconds to record
+    record_secs = 10 # seconds to record
     dev_index = 1 # device index found by p.get_device_info_by_index(ii)
     wav_output_filename = uuid.uuid4().hex + '.wav' # name of .wav file
     
