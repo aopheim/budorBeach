8a621f756b0317969cbfc759ca937d6042b2c471
aopheim <37472517+aopheim@users.noreply.github.com>
2022-05-04 23:18:22 +0200

    Suppress error messages from pyaudio

--- AudioService/recordAudioLinux.py
+++ AudioService/recordAudioLinux.py
@@ -1,7 +1,23 @@
+#!/usr/bin/env python
+from ctypes import *
+import pyaudio
 import uuid
 import wave
 import pyaudio
 
+# Suppressing error messages as suggested here: https://stackoverflow.com/questions/7088672/pyaudio-working-but-spits-out-error-messages-each-time
+# From alsa-lib Git 3fd4ab9be0db7c7430ebd258f2717a976381715d
+# $ grep -rn snd_lib_error_handler_t
+# include/error.h:59:typedef void (*snd_lib_error_handler_t)(const char *file, int line, const char *function, int err, const char *fmt, ...) /* __attribute__ ((format (printf, 5, 6))) */;
+# Define our error handler type
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+def py_error_handler(filename, line, function, err, fmt):
+  pass
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+asound = cdll.LoadLibrary('libasound.so')
+# Set error handler
+asound.snd_lib_error_set_handler(c_error_handler)
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
