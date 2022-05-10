ae621d7940d8911667bc6b178b05a31d2f383dd0
Adrian Opheim <adrian.opheim@fiizk.com>
2022-05-10 10:27:38 +0200

    Add Id as Key to SpeciesRecognitionModel

--- Shared/Models/SpeciesRecognitionModel.cs
+++ Shared/Models/SpeciesRecognitionModel.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Shared.Models
 {
     public class SpeciesRecognitionModel
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
         public double Confidence { get; set; }
         public string LatinName { get; set; }
         public string EnglishName { get; set; }
