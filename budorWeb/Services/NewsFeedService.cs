9dd01c810011e9b07fbb7ee56eaf6f3ef1b0e22c
Adrian Opheim <adrian.opheim@fiizk.com>
2022-05-12 09:05:28 +0200

    Add news item

--- budorWeb/Services/NewsFeedService.cs
+++ budorWeb/Services/NewsFeedService.cs
@@ -305,6 +305,20 @@ namespace budorWeb.Services
                         "https://budorbeach.blob.core.windows.net/images-thumbnails/2022-5-8/11-59-34.jpg",
                         "https://budorbeach.blob.core.windows.net/images-thumbnails/2022-5-10/6-59-34.jpg"
                     }
+                },
+                new NewsFeedEntry
+                {
+                    Id = "v",
+                    Date = new DateTime(2022, 05, 12),
+                    Header = "Jomfrufødsel fra Herr Fluesnapper?",
+                    Body =
+                        "I dag kom det egg i fuglekassa! Men hvem som har lagt egget er et mysterium. Vi har ikke sett noe til besøk fra Fru Fluesnapper, og selve fødselen må ha skjedd veldig fort i morgentimene. Kameraet har bare greid å plukke opp det svart-hvite hodet til Herr Fluesnapper, så sannsynligvis må dette være verdens første dokumenterte jomfrufødsel fra enn mannlig fluesnapper. Sensasjon!",
+                    ImageUrls = new List<string>
+                    {
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2022-5-12/3-29-34.jpg",
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2022-5-12/2-59-34.jpg",
+                        "https://budorbeach.blob.core.windows.net/images-thumbnails/2022-5-12/3-14-34.jpg"
+                    }
                 }
             }.OrderByDescending(e => e.Date).ToList();
         }
