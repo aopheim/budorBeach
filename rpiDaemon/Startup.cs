f3e126075c6134f5fb8c6c733a2ae92efe162b01
Adrian Opheim <adrian.opheim@fiizk.com>
2022-05-13 21:45:58 +0200

    Fix false unit tests, fix guid bug with hashes

--- rpiDaemon/Startup.cs
+++ rpiDaemon/Startup.cs
@@ -51,15 +51,15 @@ namespace rpiDaemon
             services.AddQuartz(q =>
             {
                 q.UseJobFactory<JobFactory>();
-                q.AddJobAndTrigger<GetBme280SensorReadingsJob>(GlobalConstants.SecondJobs,
-                    GlobalConstants.Bme280Trigger,
-                    _environment.IsDevelopment() ? TimeSpan.FromSeconds(2) : TimeSpan.FromSeconds(10));
+                // q.AddJobAndTrigger<GetBme280SensorReadingsJob>(GlobalConstants.SecondJobs,
+                //     GlobalConstants.Bme280Trigger,
+                //     _environment.IsDevelopment() ? TimeSpan.FromSeconds(2) : TimeSpan.FromSeconds(10));
                 q.AddJobAndTrigger<TakePictureJob>(GlobalConstants.SecondJobs, GlobalConstants.PictureTrigger,
                     _environment.IsDevelopment() ? TimeSpan.FromSeconds(10) : TimeSpan.FromHours(4));
                 // q.AddJobAndTrigger<TakeVideoJob>(GlobalConstants.SecondJobs, GlobalConstants.VideoRecordingTrigger,
                 //     null, DateTime.UtcNow.AddSeconds(20));
-                q.AddJobAndTrigger<GetProximityJob>(GlobalConstants.SecondJobs, GlobalConstants.ProximityTrigger,
-                    TimeSpan.FromSeconds(5));
+                // q.AddJobAndTrigger<GetProximityJob>(GlobalConstants.SecondJobs, GlobalConstants.ProximityTrigger,
+                //     TimeSpan.FromSeconds(5));
                 q.AddJobAndTrigger<TakeAudioRecordingJob>(GlobalConstants.SecondJobs,
                     GlobalConstants.AudioRecordingTrigger, null);
                 q.AddJobAndTrigger<AnalyzeAudioRecordingsJob>(GlobalConstants.SecondJobs,
