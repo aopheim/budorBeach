f333ee36a547eb64c4104e6ea8397139d4e636bc
aopheim <37472517+aopheim@users.noreply.github.com>
2022-05-26 17:30:17 +0200

    Use while loop i C#, not Python. Add Seq

--- AudioService/recordAudioWindows.py
+++ AudioService/recordAudioWindows.py
@@ -6,10 +6,10 @@ import os
 
 fs = 44100  # Sample rate
 seconds = 10  # Duration of recording
-while True:
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-    sd.wait()  # Wait until recording is finished
-    fileName = str(uuid.uuid1())
-    filePath = os.getenv('APPDATA') + '\BudorBeach\\' + fileName + '.wav' 
-    wf.write(filePath, fs, myrecording)  # Save as WAV file 
-    print('Saved recording to ', filePath)
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+fileName = str(uuid.uuid1())
+filePath = os.getenv('APPDATA') + '\BudorBeach\\' + fileName + '.wav' 
+wf.write(filePath, fs, myrecording)  # Save as WAV file 
+print('Saved recording to ', filePath)
