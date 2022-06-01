d3aae07e4b56fb0e0db22a9b4710506039b681e7
Adrian Opheim <adrian.opheim@fiizk.com>
2022-06-01 19:09:53 +0200

    Add video stream

--- CameraService/CameraService.Streaming.cs
+++ CameraService/CameraService.Streaming.cs
@@ -19,7 +19,7 @@ namespace CameraService
             }
 
             var arguments =
-                "-c \"raspivid -o - -t 0 -w 800 -h 600 -fps 12  | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp = rtsp://:80/}' :demux=h264\"";
+                "-c \"raspivid -o - -t 0 -w 800 -h 600 -fps 12 --nopreview  | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp = rtsp://:80/}' :demux=h264\"";
             Process process = new();
             ProcessStartInfo startInfo = new()
             {
@@ -32,6 +32,7 @@ namespace CameraService
             process.Start();
             await process.WaitForExitAsync(
                 CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _streamingTimeOut.Token).Token);
+            process.Exited += (sender, args) => { _logger.LogInformation("Cancelling video stream..."); };
         }
     }
 }
