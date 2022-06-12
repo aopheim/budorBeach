89cccf4b3f9aa91d1f73253b16140cb83dbf05fb
Adrian Opheim <adrian.opheim@fiizk.com>
2022-06-12 22:27:52 +0200

    Write audioRecording to audioRecordings folder

--- AudioService/recordAudioLinux.py
+++ AudioService/recordAudioLinux.py
@@ -26,7 +26,7 @@ samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
 record_secs = 10 # seconds to record
 dev_index = 1 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = str(uuid.uuid1()) + '.wav' # name of .wav file
+wav_output_filename = 'home/pi/audioRecordings/' + str(uuid.uuid1()) + '.wav' # name of .wav file
 
 audio = pyaudio.PyAudio()  # create pyaudio instantiation
 # create pyaudio stream
