73502c59be7124d248f142761b2eba9797b212ce
aopheim <adrian-o@online.no>
2023-12-03 11:31:48 +0100

    Add missing method

--- AudioService/recordAudioLinuxDebug.py
+++ AudioService/recordAudioLinuxDebug.py
@@ -19,7 +19,8 @@ import json
 ERROR_HANDLER_FUNC = CFUNCTYPE(
     None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
-
+def py_error_handler(filename, line, function, err, fmt):
+    pass
 c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
 asound = cdll.LoadLibrary('libasound.so')
