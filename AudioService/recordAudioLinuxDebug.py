d95741faa0a95049f8ba2107dc1af52530ca98b9
aopheim <adrian-o@online.no>
2023-12-03 11:35:56 +0100

    Is error handler needed?

--- AudioService/recordAudioLinuxDebug.py
+++ AudioService/recordAudioLinuxDebug.py
@@ -19,13 +19,13 @@ import json
 ERROR_HANDLER_FUNC = CFUNCTYPE(
     None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
-def py_error_handler(filename, line, function, err, fmt):
-    pass
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+# def py_error_handler(filename, line, function, err, fmt):
+#     pass
+# c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
-asound = cdll.LoadLibrary('libasound.so')
-# Set error handler
-asound.snd_lib_error_set_handler(c_error_handler)
+# asound = cdll.LoadLibrary('libasound.so')
+# # Set error handler
+# asound.snd_lib_error_set_handler(c_error_handler)
 
 form_1 = pyaudio.paInt16  # 16-bit resolution
 chans = 1  # 1 channel
